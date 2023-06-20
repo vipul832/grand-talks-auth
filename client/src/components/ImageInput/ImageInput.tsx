0f9d26cf16c90fcc1a9a254e9be6dd9b1d38aef7
vipulc8 <vipul.c@simformsolutions.com>
2023-06-20 14:18:12 +0530

    test 2 for upload

--- client/src/components/ImageInput/ImageInput.tsx
+++ client/src/components/ImageInput/ImageInput.tsx
@@ -4,6 +4,7 @@ import { IKContext, IKUpload } from "imagekitio-react";
 import { UploadResponse } from "../../utils/types";
 
 const environment = import.meta.env;
+console.log(environment);
 
 type ImageInputProps = {
   image: string;
