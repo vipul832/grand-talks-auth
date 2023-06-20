b7826637b453f9bea1918b41e715982e6516f53f
vipulc8 <vipul.c@simformsolutions.com>
2023-06-20 15:15:37 +0530

    update signup page

--- client/src/components/SignUp/SignUp.tsx
+++ client/src/components/SignUp/SignUp.tsx
@@ -61,7 +61,7 @@ const SignUp = () => {
 
   return (
     <div className="min-h-screen relative flex justify-center items-center my-20">
-      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] md:w-[90%] shadow-lg flex justify-between p-10 rounded-lg">
+      <div className="absolute top-[40%] left-[50%] translate-x-[-50%] translate-y-[-50%] md:w-[90%] shadow-lg flex justify-between px-6 rounded-lg">
         <div className="lg:flex hidden ms-10 justify-center w-[50%]">
           <img
             src="https://ik.imagekit.io/fqy6m4jmj/weimages/Secure_login-bro.svg?updatedAt=1687173591904"
@@ -227,14 +227,14 @@ const SignUp = () => {
             {/* <Link to={"/signin"}>
             </Link> */}
             <Button
-              className="mt-6 border hover:border-primaryPurple text-white font-bold hover:bg-white hover:text-primaryPurple"
+              className="mt-4 border hover:border-primaryPurple text-white font-bold hover:bg-white hover:text-primaryPurple"
               color="deep-purple"
               fullWidth
               type="submit"
             >
               Register
             </Button>
-            <Typography color="gray" className="mt-4 text-center font-normal">
+            <Typography color="gray" className="mt-2 text-center font-normal">
               Already have an account?{" "}
               <Link
                 to="/signin"
